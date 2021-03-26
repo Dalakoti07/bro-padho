ebeb8ea7462a748f1c69d57a9e20e857a47b3f0f
dalakoti07 <saurabhdalakoti07@gmail.com>
2021-03-27 00:31:42 +0530

    added syllabus links

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NavBar from './components/Navbar';
 import Exam from './components/Exam';
 import TestComponent from './components/TestComponent'
 import Footer from './components/Footer';
+import Syllabus from './components/Syllabus'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' exact>
           <Home />
         </Route>
+        <Route exact path="/syllabus/:exam" render={(props)=> <Syllabus examName={props.match.params.exam}/>} />
         <Route exact path="/exams/:id" render={(props)=> <Exam examId={props.match.params.id}/>} />
         <Route exact path="/tests/:id" render={(props)=> <TestComponent examKey={props.match.params.id} />} />
       </Switch>
